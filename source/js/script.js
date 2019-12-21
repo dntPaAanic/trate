1fc9893312d233fbefb5faed44f800766c7c0dfc
mes <formecc@gmail.com>
2019-12-21 17:57:03 +0300

    Делает часть слайдера

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,46 @@
-var swiper = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper-container-1', {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  autoplay: {
+    delay: 5000,
+  },
   simulateTouch: false,
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination-1',
     clickable: true,
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next-1',
+    prevEl: '.swiper-button-prev-1',
   },
 });
+
+// var productCardSlider = new Swiper('.product-card__slider-container', {
+//   slidesPerView: 1,
+//   spaceBetween: 10,
+//   // init: false,
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   breakpoints: {
+//     640: {
+//       slidesPerView: 2,
+//       spaceBetween: 20,
+//     },
+//     768: {
+//       slidesPerView: 4,
+//       spaceBetween: 40,
+//     },
+//     1280: {
+//       slidesPerView: 3,
+//       spaceBetween: 33,
+//     },
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//
+// });
